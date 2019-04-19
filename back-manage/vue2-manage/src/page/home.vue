cd21257210c514c7ba30c2975b24befd83f56539
zhuo <13640133903@163.com>
2019-04-19 09:23:11 +0800

    [update] 后台管理页面

---
+++ back-manage/vue2-manage/src/page/home.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <head-top></head-top>
+		<section class="data_section">
+			<header class="section_title">欢迎来到大学生心理咨询系统管理页面</header>
+		</section>
+    </div>
+</template>
+
+<script>
+	import headTop from '../components/headTop'
+	import tendency from '../components/tendency'
+	import dtime from 'time-formater'
+	import {userCount, orderCount, getUserCount, getOrderCount, adminDayCount, adminCount} from '@/api/getData'
+    export default {
+    	data(){
+    		return {
+    		}
+    	},
+    	components: {
+    		headTop
+    	},
+    	mounted(){
+    	},
+        computed: {
+
+        },
+    	methods: {
+    	}
+    }
+</script>
+
+<style lang="less">
+	@import '../style/mixin';
+	.data_section{
+		padding: 20px;
+		margin-bottom: 40px;
+		.section_title{
+			text-align: center;
+			font-size: 30px;
+			margin-bottom: 10px;
+		}
+		.data_list{
+			text-align: center;
+			font-size: 14px;
+			color: #666;
+            border-radius: 6px;
+            background: #E5E9F2;
+            .data_num{
+                color: #333;
+                font-size: 26px;
+
+            }
+            .head{
+                border-radius: 6px;
+                font-size: 22px;
+                padding: 4px 0;
+                color: #fff;
+                display: inline-block;
+            }
+        }
+        .today_head{
+            background: #FF9800;
+        }
+        .all_head{
+            background: #20A0FF;
+        }
+	}
+    .wan{
+        .sc(16px, #333)
+    }
+</style>
